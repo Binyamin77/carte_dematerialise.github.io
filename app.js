65c7cd1075cad841cff00068ca2e76c057bbf479
Binyamin77 <benli1@hotmail.fr>
2025-05-07 12:22:30 +0200

    first

---
+++ app.js
@@ -0,0 +1,8 @@
+function showAlert(message) {
+    alert(message);
+}
+
+function biometricLogin() {
+    alert("Simulation de connexion avec Face ID/Touch ID. Authentification réussie !");
+    window.location.href = 'home.html';
+}
